92283ab08c66d52b55612029cf06b24dfa694079
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-03-26 16:47:07 +0800

    update

--- hpsv2.py
+++ hpsv2.py
@@ -8,6 +8,7 @@ import folder_paths
 from comfy.cli_args import args
 from PIL.PngImagePlugin import PngInfo
 import json
+import webp
 
 # set HF_ENDPOINT=https://hf-mirror.com
 class Loader:
@@ -203,6 +204,7 @@ class SaveImage:
             metadata = None
             if not args.disable_metadata:
                 metadata = PngInfo()
+                metadata.add_text("score", json.dumps(score))
                 if prompt is not None:
                     metadata.add_text("prompt", json.dumps(prompt))
                 if extra_pnginfo is not None:
@@ -220,6 +222,60 @@ class SaveImage:
             counter += 1
 
         return { "ui": { "images": results } }
+    
+class SaveWebpImage:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+        self.compress_level = 4
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+                "required": 
+                {
+                    "images": ("IMAGE", ),
+                    "filename_prefix": ("STRING", {"default": "Hjh"}),
+                    "score": ("PS_SCORES",),
+                },
+                "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
+            }
+
+    RETURN_TYPES = ()
+    FUNCTION = "save_webp_images"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "Haojihui/HPSv2"
+
+    def save_webp_images(self, images, filename_prefix="Hjh", score="", prompt=None, extra_pnginfo=None):
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
+        results = list()
+        for (batch_number, image) in enumerate(images):
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            metadata = None
+            if not args.disable_metadata:
+                metadata = PngInfo()
+                if prompt is not None:
+                    metadata.add_text("prompt", json.dumps(prompt))
+                if extra_pnginfo is not None:
+                    for x in extra_pnginfo:
+                        metadata.add_text(x, json.dumps(extra_pnginfo[x]))
+
+            filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
+            file = f"{filename_with_batch_num}_{counter:05}_{score}_.webp"
+            webp.save_image(img, os.path.join(full_output_folder, file), quality=80)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": self.type
+            })
+            counter += 1
+
+        return { "ui": { "images": results } }
 
 NODE_CLASS_MAPPINGS = {
     "HaojihuiHPSv2Loader": Loader,
@@ -227,6 +283,7 @@ NODE_CLASS_MAPPINGS = {
     "HaojihuiHPSv2TextProcessor": TextProcessor,
     "HaojihuiHPSv2ImageScore": ImageScore,
     "HaojihuiHPSv2SaveImage": SaveImage,
+    "HaojihuiHPSv2SaveWebpImage": SaveWebpImage,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -235,4 +292,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "HaojihuiHPSv2TextProcessor": "Text Processor",
     "HaojihuiHPSv2ImageScore": "ImageScore",
     "HaojihuiHPSv2SaveImage": "SaveImage",
+    "HaojihuiHPSv2SaveWebpImage": "SaveWebpImage",
 }
