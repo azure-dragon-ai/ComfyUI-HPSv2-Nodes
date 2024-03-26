56e37db2f1c9fdb42962e75a1d1498687ef3f641
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-03-26 14:12:58 +0800

    update

--- hpsv2.py
+++ hpsv2.py
@@ -163,12 +163,62 @@ class ImageScore:
 
         return (scores_str, scores)
 
+class SaveImage:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+        self.compress_level = 4
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": 
+                    {"images": ("IMAGE", ),
+                     "filename_prefix": ("STRING", {"default": "ComfyUI"})},
+                "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
+                }
+
+    RETURN_TYPES = ()
+    FUNCTION = "save_images"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "image"
+
+    def save_images(self, images, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None):
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
+        results = list()
+        for (batch_number, image) in enumerate(images):
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            metadata = None
+            if not args.disable_metadata:
+                metadata = PngInfo()
+                if prompt is not None:
+                    metadata.add_text("prompt", json.dumps(prompt))
+                if extra_pnginfo is not None:
+                    for x in extra_pnginfo:
+                        metadata.add_text(x, json.dumps(extra_pnginfo[x]))
+
+            filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
+            file = f"{filename_with_batch_num}_{counter:05}_.png"
+            img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": self.type
+            })
+            counter += 1
+
+        return { "ui": { "images": results } }
 
 NODE_CLASS_MAPPINGS = {
     "HaojihuiHPSv2Loader": Loader,
     "HaojihuiHPSv2ImageProcessor": ImageProcessor,
     "HaojihuiHPSv2TextProcessor": TextProcessor,
     "HaojihuiHPSv2ImageScore": ImageScore,
+    "HaojihuiHPSv2SaveImage": SaveImage,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -176,4 +226,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "HaojihuiHPSv2ImageProcessor": "Image Processor",
     "HaojihuiHPSv2TextProcessor": "Text Processor",
     "HaojihuiHPSv2ImageScore": "ImageScore",
+    "HaojihuiHPSv2SaveImage": "SaveImage",
 }
